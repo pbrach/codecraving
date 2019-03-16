99cb91c6c777211a9d606f72289980cfbdca8d12
Philipp Brachvogel <philipp-brachvogel@web.de>
2019-03-17 00:30:49 +0100

    simple fun function: filter the input for new words and display only these. the rx-linq filters are quite nice for that

--- AvaloReact/ViewModels/MainWindowViewModel.cs
+++ AvaloReact/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
-using System.Reactive.Disposables;
+using System.Linq;
 using System.Reactive.Linq;
 using ReactiveUI;
 
@@ -10,7 +10,7 @@ namespace AvaloReact.ViewModels
     {
         private string _output = "First Output line";
 
-        public string Input => "<type here your input>";
+        public string Input => "";
 
         public string Output
         {
@@ -21,12 +21,25 @@ namespace AvaloReact.ViewModels
         public void AddInputObservable(IObservable<string> textObservable)
         {
             textObservable
-                .Throttle(TimeSpan.FromMilliseconds(300))
+                .Throttle(TimeSpan.FromMilliseconds(800))
                 .Where(x => x.Length > 2)
+                .Select(s =>
+                {
+                    var s1 = s.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+                    var s2 = Output.Split(new char[0], StringSplitOptions.RemoveEmptyEntries);
+
+                    var newWords = s1.Where( t => !s2.Contains(t) && !string.IsNullOrEmpty(t));
+                    return string.Join(" ", newWords);
+                })
                 .Subscribe(s =>
                 {
-                    Console.WriteLine($"ENTERED STRING: {s}");
-                    Output += Environment.NewLine + s;
+                    if(string.IsNullOrEmpty(s)) return;
+
+                    foreach (var newWord in s.Split(" ").Distinct())
+                    {
+                        Console.WriteLine($"ENTERED STRING: {newWord}");
+                        Output += Environment.NewLine + newWord;
+                    }
                 });
         }
     }
