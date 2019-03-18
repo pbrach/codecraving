4ca73b9f95e014f2780f3a0491f22a331aafbfa1
Philipp Brachvogel <philipp-brachvogel@web.de>
2019-03-18 21:09:56 +0100

    .net core x-plattform app, html5 based, using system wide webkit or MSHTML, example app works without a local http server

---
+++ DesktopWebApp/App/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+
+using Webview;
+
+namespace DesktopWebApp
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            const string html = 
+@"
+<html>
+<body>
+    <h1>Eval</h1>
+    <div id='content'></div>
+
+    <input type='text' id='nameInput' placeholder='enter name'/>
+    <button onclick='MyButtonClick()'>Do it!</button>
+    <p id='display'></p>
+
+    <script>
+        function MyButtonClick() {
+            window.external.invoke('click:'+document.getElementById('nameInput').value);
+        }
+    </script>
+</body>
+</html>";
+            var webview = new WebviewBuilder("Eval", Content.FromHtml(html))
+                .WithSize(new Size(1024, 768))
+                .WithInvokeCallback(CallbackHandler)
+                .Debug()
+                .Build();
+
+            var tick = 0;
+            while (webview.Loop() == 0)
+            {
+                if (tick % 1000 == 0)
+                {
+                    webview.Eval(@"document.getElementById('content').textContent = 'Time:" + DateTime.Now + "'");
+                }
+
+                tick++;
+                GC.Collect();
+            }
+        }
+
+        private static void CallbackHandler(Webview.Webview webview, string args)
+        {
+            if (args.StartsWith("click"))
+            {
+                var name = args.Split(":")[1];
+                webview.Eval($@"document.getElementById('display').textContent = 'Hello {name}'");
+            }
+        }
+    }
+}
